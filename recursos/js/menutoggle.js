93666dd8026f9210c5f131255bd579eb2881bd10
luck95 <39856678+luck95@users.noreply.github.com>
2018-10-15 15:15:53 -0500

    Delete menutoggle.js

--- recursos/js/menutoggle.js
+++
@@ -1,4 +0,0 @@
-$('.menu-toggle > ul > li > span').click(function(){
-    $(this).toggleClass('sactivate');
-    $(this).next().slideToggle().css({'display':'flex'}).parent().siblings().find('> span').removeClass().next().slideUp();
-})
